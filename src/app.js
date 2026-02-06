52b24a52c7fa165a805f1de0758bdc73274df2cb
Neo1917 <jhongomez191219@gmail.com>
2026-02-06 04:27:25 +0000

    carta aleatorias Jhon

--- src/app.js
+++ src/app.js
@@ -1,11 +1,56 @@
-import "bootstrap";
-import "./style.css";
+const suits = ["♦", "♥", "♠", "♣"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
+const topSuit = document.getElementById("top-suit");
+const value = document.getElementById("value");
+const bottomSuit = document.getElementById("bottom-suit");
+const timerText = document.getElementById("timer");
+const btn = document.getElementById("newCardBtn");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let timeLeft = 10;
+let interval;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateCard() {
+  // Elegir icono random
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  let val = values[Math.floor(Math.random() * values.length)];
+
+  // Aplicar valores
+  topSuit.innerHTML = suit;
+  value.innerHTML = val;
+  bottomSuit.innerHTML = suit;
+
+  // Color rojo para corazones y diamantes
+  if (suit === "♦" || suit === "♥") {
+    topSuit.style.color = "red";
+    value.style.color = "red";
+    bottomSuit.style.color = "red";
+  } else {
+    topSuit.style.color = "black";
+    value.style.color = "black";
+    bottomSuit.style.color = "black";
+  }
+
+  // Reiniciar el temporizador
+  resetTimer();
+}
+
+function resetTimer() {
+  clearInterval(interval);
+  timeLeft = 10;
+  timerText.innerHTML = timeLeft;
+
+  interval = setInterval(() => {
+    timeLeft--;
+    timerText.innerHTML = timeLeft;
+
+    if (timeLeft <= 0) {
+      generateCard();
+    }
+  }, 1000);
+}
+
+btn.addEventListener("click", generateCard);
+
+// Ejecutar primera carta
+generateCard();
